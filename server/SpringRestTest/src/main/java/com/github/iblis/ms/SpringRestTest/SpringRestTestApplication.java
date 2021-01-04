6bddb69b41711a55215af2840d2de285cfaf78b4
iblis <marcin.serwach>
2021-01-04 23:15:52 +0100

    init

---
+++ server/SpringRestTest/src/main/java/com/github/iblis/ms/SpringRestTest/SpringRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.iblis.ms.SpringRestTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTestApplication.class, args);
+	}
+
+}
---
+++ server/SpringRestTest/src/test/java/com/github/iblis/ms/SpringRestTest/SpringRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iblis.ms.SpringRestTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
