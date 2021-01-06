52280e3a52a4fa55fd840c27a617c5290e27ce78
iblis <marcin.serwach>
2021-01-06 15:15:30 +0100

    ios init

---
+++ ios/IosRestTestApp/IosRestTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosRestTestApp
+//
+//  Created by iblis on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
